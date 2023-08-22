b94414915bc765015c5d2fca5916fa600e08628b
Razvan Vancea <razvan.vancea@zitec.com>
2023-08-22 17:38:22 +0300

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,11 @@
+/* eslint-env node */
 module.exports = {
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/stylistic',
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	root: true,
-	parserOptions: {
-		project: true,
-		tsconfigRootDir: __dirname,
-	},
-};
+    extends: ['eslint:recommended', 
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+    'prettier'
+  ],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+  };
